9633b7aba81edb64db55a01c3ebdc494e134557b
Juanma Romero <pro@juanmaroni.me>
2021-12-21 13:54:36 +0100

    Changing how to call mod days

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 mod input_handler;
 mod days;
-use days::{day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21};
 
 fn main() {
-    day01::day01_answer();
-    day02::day02_answer();
-    day03::day03_answer();
-    day04::day04_answer();
-    day05::day05_answer();
-    day06::day06_answer();
-    day07::day07_answer();
-    day08::day08_answer();
-    day09::day09_answer();
-    day10::day10_answer();
-    day11::day11_answer();
-    day12::day12_answer();
-    day13::day13_answer();
-    day14::day14_answer();
-    day15::day15_answer();
-    day16::day16_answer();
-    day17::day17_answer();
-    day18::day18_answer();
-    day19::day19_answer();
-    day20::day20_answer();
-    day21::day21_answer();
+    days::day01::day01_answer();
+    days::day02::day02_answer();
+    days::day03::day03_answer();
+    days::day04::day04_answer();
+    days::day05::day05_answer();
+    days::day06::day06_answer();
+    days::day07::day07_answer();
+    days::day08::day08_answer();
+    days::day09::day09_answer();
+    days::day10::day10_answer();
+    days::day11::day11_answer();
+    days::day12::day12_answer();
+    days::day13::day13_answer();
+    days::day14::day14_answer();
+    days::day15::day15_answer();
+    days::day16::day16_answer();
+    days::day17::day17_answer();
+    days::day18::day18_answer();
+    days::day19::day19_answer();
+    days::day20::day20_answer();
+    days::day21::day21_answer();
 }
