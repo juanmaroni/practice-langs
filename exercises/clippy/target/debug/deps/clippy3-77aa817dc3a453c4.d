d540f0c7e3ed0ee2457ee07b4605bdcbff768246
Juanma Romero <pro@juanmaroni.me>
2022-07-25 21:53:45 +0200

    Clippy

---
+++ exercises/clippy/target/debug/deps/clippy3-77aa817dc3a453c4.d
@@ -0,0 +1,7 @@
+/home/juanmaroni/projects/my-rustlings/exercises/clippy/target/debug/deps/clippy3-77aa817dc3a453c4.rmeta: clippy3.rs
+
+/home/juanmaroni/projects/my-rustlings/exercises/clippy/target/debug/deps/clippy3-77aa817dc3a453c4.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
