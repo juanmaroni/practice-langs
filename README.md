ad4a4fecaff07f7d248aa67f268512bf1ea64aa1
Juanma Romero <pro@juanmaroni.me>
2022-12-01 15:24:17 +0100

    [Day 1] Complete and refactored

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Advent of Code 2022
-
 Yup, Rust again.
 
 ## Notes
-I overengineered a bit, for learning purposes.
+I overengineered a bit, for learning purposes. Don't overthink about it.
+
+### Day 1
+I knew that I would need to reuse something from the P1 on P2, but I didnt know what,
+so I solved P1 first. Then I realized what I could reuse and decided to solve P2 before
+refactoring.
+
+Reused an iterator. I could have reused something easier (or nothing), but was curious about how to do it.
--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,35 +1,69 @@
 // Advent of Code 2022
-// Day 1: TITLE OF THE DAY
+// Day 1: Calorie Counting
 
 use std::io::BufRead;
-
+use itertools::Itertools;
 use aoc2022::{Day, Part, Answer};
 
 const FILE: &str = "inputs/real/day01_input.txt";
 
 pub fn print_answers() {
     let mut day = Day::new(1, FILE.to_string());
-    day.first_answer = Some(Answer::Num(0));
-    day.second_answer = Some(Answer::Num(0));
 
-    // let input = parse_input(day);
+    let input = parse_input(&day);
+    let iter_sums = sum_and_sort_calories_by_elf(&input);
+    let top3 = get_top3(iter_sums);
+
+    day.first_answer = Some(Answer::Num(get_higher_calories_one_elf(&top3)));
+    day.second_answer = Some(Answer::Num(sum_top3(&top3)));
 
     day.print_answer(day.day_number, Part::One, &day.first_answer);
     day.print_answer(day.day_number, Part::Two, &day.second_answer);
 }
 
-fn parse_input(day: Day) {
+fn parse_input(day: &Day) -> Vec<Vec<u32>> {
+    let mut calories: Vec<Vec<u32>> = Vec::new();
+    let mut elf: Vec<u32> = Vec::new();
+
     for line in day.read_file().lines() {
-        
+        let content = line.unwrap();
+
+        if !content.is_empty() {
+            elf.push(content.parse::<u32>().unwrap());
+        } else {
+            calories.push(elf);
+            elf = Vec::new();
+        }
     }
+
+    // Add collected data before EOF
+    calories.push(elf);
+
+    calories
 }
 
-fn calc_first_answer(values: Vec<u32>) -> u32 {
-    todo!()
+// Part 1
+fn get_higher_calories_one_elf(calorie_sums: &Vec<u32>) -> u64 {
+    calorie_sums[0] as u64
 }
 
-fn calc_second_answer(values: Vec<u32>) -> u32 {
-    todo!()
+// Part 2
+fn sum_top3(calorie_sums: &Vec<u32>)-> u64 {
+    calorie_sums.iter()
+        .sum::<u32>() as u64
+}
+
+// Create a sorted iterator with the calorie sum of each elf
+fn sum_and_sort_calories_by_elf<'a>(calories: &'a Vec<Vec<u32>>) -> impl Iterator<Item = u32> + 'a {
+    calories.iter()
+        .map(|elf| elf.iter().sum::<u32>())
+        .sorted_by(|a, b| Ord::cmp(b, a))
+}
+
+// Get Top 3 higher calorie sums to solve both parts
+fn get_top3<'a>(calorie_sums: impl Iterator<Item = u32> + 'a) -> Vec<u32> {
+    calorie_sums.take(3)
+        .collect::<Vec<u32>>()
 }
 
 #[cfg(test)]
@@ -39,20 +73,30 @@ mod tests {
     const FILE: &str = "inputs/tests/day01_input_test.txt";
 
     #[test]
-    fn day01_part1_test() { // Change Day01
+    fn day01_part1_test() {
         let mut day = Day::new(1, FILE.to_string());
-        day.first_answer = Some(Answer::Num(0));
-        day.second_answer = Some(Answer::Num(0));
 
-        assert_eq!(7, 7);
+        let input = parse_input(&day);
+        let iter_sums = sum_and_sort_calories_by_elf(&input);
+        let top3 = get_top3(iter_sums);
+        let ans = get_higher_calories_one_elf(&top3);
+
+        assert_eq!(ans, 24000);
+
+        day.first_answer = Some(Answer::Num(ans));
     }
 
     #[test]
-    fn day01_part2_test() { // Change Day01
+    fn day01_part2_test() {
         let mut day = Day::new(1, FILE.to_string());
-        day.first_answer = Some(Answer::Num(0));
-        day.second_answer = Some(Answer::Num(0));
 
-        assert_eq!(5, 5);
+        let input = parse_input(&day);
+        let iter_sums = sum_and_sort_calories_by_elf(&input);
+        let top3 = get_top3(iter_sums);
+        let ans = sum_top3(&top3);
+
+        assert_eq!(ans, 45000);
+
+        day.second_answer = Some(Answer::Num(ans));
     }
 }
