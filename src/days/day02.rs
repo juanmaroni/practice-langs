85ffa9c85d6d88795964cdac0f4c16e19f326303
Juanma Romero <pro@juanmaroni.me>
2022-12-02 09:56:32 +0100

    [Day 2] Part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -2,7 +2,6 @@
 // Day 2: Rock Paper Scissors
 
 use std::io::BufRead;
-
 use aoc2022::{Day, Part, Answer};
 
 const FILE: &str = "inputs/real/day02_input.txt";
@@ -12,8 +11,8 @@ pub fn print_answers() {
 
     let strat_guide = parse_input(&day);
 
-    day.first_answer = Some(Answer::Num(calc_score(&strat_guide) as u64));
-    day.second_answer = Some(Answer::Num(0));
+    day.first_answer = Some(Answer::Num(calc_wrong_score(&strat_guide) as u64));
+    day.second_answer = Some(Answer::Num(calc_right_score(&strat_guide) as u64));
 
     day.print_answer(day.day_number, Part::One, &day.first_answer);
     day.print_answer(day.day_number, Part::Two, &day.second_answer);
@@ -34,16 +33,17 @@ fn parse_input(day: &Day) -> Vec<(char, char)> {
 }
 
 // Part 1
-fn calc_score(strat_guide: &Vec<(char, char)>) -> u32 {
-    strat_guide.iter().map(|play| match_play(play)).sum()
+fn calc_wrong_score(strat_guide: &Vec<(char, char)>) -> u32 {
+    strat_guide.iter().map(|play| match_play_wrong(play)).sum()
 }
 
 // Part 2
-fn calc_second_answer(strat_guide: &Vec<(char, char)>) -> u32 {
-    todo!()
+fn calc_right_score(strat_guide: &Vec<(char, char)>) -> u32 {
+    strat_guide.iter().map(|play| match_play_right(play)).sum()
 }
 
-fn match_play(play: &(char, char)) -> u32 {
+// This is so dirty... XD
+fn match_play_wrong(play: &(char, char)) -> u32 {
     match play {
         ('A', 'X') => 1 + 3,
         ('B', 'X' ) => 1,
@@ -58,6 +58,21 @@ fn match_play(play: &(char, char)) -> u32 {
     }
 }
 
+fn match_play_right(play: &(char, char)) -> u32 {
+    match play {
+        ('A', 'Z') => 6 + 2,
+        ('B', 'Z' ) => 6 + 3,
+        ('C', 'Z') => 6 + 1,
+        ('A', 'Y') => 3 + 1,
+        ('B', 'Y' ) => 3 + 2,
+        ('C', 'Y') => 3 + 3,
+        ('A', 'X') => 3,
+        ('B', 'X' ) => 1,
+        ('C', 'X') => 2,
+        _ => panic!("Unrecognized play!"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,7 +83,7 @@ mod tests {
     fn day02_part1_test() {
         let mut day = Day::new(2, FILE.to_string());
         let strat_guide = parse_input(&day);
-        let ans = calc_score(&strat_guide);
+        let ans = calc_wrong_score(&strat_guide);
 
         assert_eq!(ans, 15);
 
@@ -78,10 +93,11 @@ mod tests {
     #[test]
     fn day02_part2_test() {
         let mut day = Day::new(2, FILE.to_string());
-        
+        let strat_guide = parse_input(&day);
+        let ans = calc_right_score(&strat_guide);
 
-        assert_eq!(5, 5);
+        assert_eq!(ans, 12);
 
-        day.second_answer = Some(Answer::Num(0));
+        day.second_answer = Some(Answer::Num(ans as u64));
     }
 }
