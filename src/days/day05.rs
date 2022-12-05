0d2ca11e3936831daf9f3d152b7564ddab7a55f8
Juanma Romero <pro@juanmaroni.me>
2022-12-05 12:54:46 +0100

    [Day 5] Part 2

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -18,8 +18,8 @@ pub fn print_answers() {
 
     let (mut stacks, rearrangements) = parse_input(&day, 9);
     
-    day.first_answer = Some(Answer::Str(get_top_message(&mut stacks, rearrangements)));
-    day.second_answer = Some(Answer::Num(0));
+    day.first_answer = Some(Answer::Str(get_top_message(&mut stacks.clone(), &rearrangements)));
+    day.second_answer = Some(Answer::Str(get_correct_message(&mut stacks, &rearrangements)));
 
     day.print_answer(day.day_number, Part::One, &day.first_answer);
     day.print_answer(day.day_number, Part::Two, &day.second_answer);
@@ -73,7 +73,7 @@ fn parse_input(day: &Day, size: u8) -> (Vec<Vec<char>>, Vec<RearrangementProc>) 
 }
 
 // Part 1
-fn get_top_message(stacks: &mut Vec<Vec<char>>, rearrangements: Vec<RearrangementProc>) -> String {
+fn get_top_message(stacks: &mut Vec<Vec<char>>, rearrangements: &Vec<RearrangementProc>) -> String {
     for rp in rearrangements {
         execute_rearrangement(stacks, &rp)
     }
@@ -82,8 +82,12 @@ fn get_top_message(stacks: &mut Vec<Vec<char>>, rearrangements: Vec<Rearrangemen
 }
 
 // Part 2
-fn calc_second_answer(values: Vec<u32>) -> u32 {
-    todo!()
+fn get_correct_message(stacks: &mut Vec<Vec<char>>, rearrangements: &Vec<RearrangementProc>) -> String {
+    for rp in rearrangements {
+        execute_rearrangement_9001(stacks, &rp)
+    }
+
+    stacks.iter().map(|stack| stack[stack.len() - 1]).collect()
 }
 
 // Helping function Part 1
@@ -92,6 +96,20 @@ fn execute_rearrangement(stacks: &mut Vec<Vec<char>>, rp: &RearrangementProc) {
         let e = stacks[rp.from as usize].pop().unwrap();
         stacks[rp.to as usize].push(e);
     }
+}
+
+// Helping function for Part 2
+fn execute_rearrangement_9001(stacks: &mut Vec<Vec<char>>, rp: &RearrangementProc) {
+    let mut elems: Vec<char> = Vec::new();
+    
+    for _ in 0..rp.quantity {
+        elems.insert(0, stacks[rp.from as usize].pop().unwrap());
+    }
+
+    for e in elems {
+        stacks[rp.to as usize].push(e);
+    }
+    
 }
 
 #[cfg(test)]
@@ -104,7 +122,7 @@ mod tests {
     fn day05_part1_test() {
         let mut day = Day::new(5, FILE.to_string());
         let (mut stacks, rearrangements) = parse_input(&day, 3);
-        let ans = get_top_message(&mut stacks, rearrangements);
+        let ans = get_top_message(&mut stacks, &rearrangements);
 
         assert_eq!(&ans, "CMZ");
 
@@ -114,10 +132,11 @@ mod tests {
     #[test]
     fn day05_part2_test() {
         let mut day = Day::new(5, FILE.to_string());
-        
+        let (mut stacks, rearrangements) = parse_input(&day, 3);
+        let ans = get_correct_message(&mut stacks, &rearrangements);
 
-        assert_eq!(5, 5);
+        assert_eq!(&ans, "MCD");
 
-        day.second_answer = Some(Answer::Num(0));
+        day.second_answer = Some(Answer::Str(ans));
     }
 }
