8988f6fb1906f6951e3662d6b409943d2110579b
Juanma Romero <pro@juanmaroni.me>
2022-12-08 11:18:53 +0100

    [Day 8] Part 2

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -2,7 +2,6 @@
 // Day 8: Treetop Tree House
 
 use std::io::BufRead;
-
 use aoc2022::{Day, Part, Answer};
 
 const FILE: &str = "inputs/real/day08_input.txt";
@@ -13,7 +12,7 @@ pub fn print_answers() {
     let tree_grid = parse_input(&day);
 
     day.first_answer = Some(Answer::Num(count_visible_trees(&tree_grid) as u64));
-    day.second_answer = Some(Answer::Num(0));
+    day.second_answer = Some(Answer::Num(get_highest_score(&tree_grid)));
 
     day.print_answer(day.day_number, Part::One, &day.first_answer);
     day.print_answer(day.day_number, Part::Two, &day.second_answer);
@@ -48,8 +47,15 @@ fn count_visible_trees(tree_grid: &Vec<Vec<u8>>) -> u16 {
 }
 
 // Part 2
-fn calc_second_answer(tree_grid: &Vec<Vec<u8>>) -> u16 {
-    todo!()
+fn get_highest_score(tree_grid: &Vec<Vec<u8>>) -> u64 {
+    let grid_len_x = tree_grid.len() - 1;
+    let grid_len_y = tree_grid[0].len() - 1;
+
+    (1..grid_len_y).map(|y| (1..grid_len_x).map(|x| calc_score(x, grid_len_x, y, grid_len_y, tree_grid))
+        .max()
+        .unwrap())
+    .max()
+    .unwrap()
 }
 
 // Helping function for Part 1
@@ -92,6 +98,50 @@ fn check_viewable(x: usize, grid_len_x: usize, y: usize, grid_len_y: usize, tree
     non_viewable < 4
 }
 
+// Helping function for Part 2
+fn calc_score(x: usize, grid_len_x: usize, y: usize, grid_len_y: usize, tree_grid: &Vec<Vec<u8>>) -> u64 {
+    let elem = tree_grid[x][y];
+    let mut scores: [u64; 4] = [0; 4];
+
+    // Left
+    for xs in (0..x).rev() {
+        scores[0] += 1;
+
+        if elem <= tree_grid[xs][y] {
+            break;
+        }
+    }
+
+    // Right
+    for xs in x+1..=grid_len_x {
+        scores[1] += 1;
+
+        if elem <= tree_grid[xs][y] {
+            break;
+        }
+    }
+
+    // Up
+    for ys in (0..y).rev() {
+        scores[2] += 1;
+
+        if elem <= tree_grid[x][ys] {
+            break;
+        }
+    }
+
+    // Down
+    for ys in y+1..=grid_len_y {
+        scores[3] += 1;
+
+        if elem <= tree_grid[x][ys] {
+            break;
+        }
+    }
+
+    scores.iter().product()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -112,10 +162,11 @@ mod tests {
     #[test]
     fn day08_part2_test() {
         let mut day = Day::new(8, FILE.to_string());
-        
+        let tree_grid = parse_input(&day);
+        let ans = get_highest_score(&tree_grid);
 
-        assert_eq!(5, 5);
+        assert_eq!(ans, 8);
 
-        day.second_answer = Some(Answer::Num(0));
+        day.second_answer = Some(Answer::Num(ans));
     }
 }
