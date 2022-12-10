871725ab33012bb9152ff6c9c9e914dfb632b237
Juanma Romero <pro@juanmaroni.me>
2022-12-10 13:34:14 +0100

    [Day 10] Part 2

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -3,6 +3,7 @@
 
 use std::io::BufRead;
 use aoc2022::{Day, Part, Answer};
+use itertools::Itertools;
 
 const FILE: &str = "inputs/real/day10_input.txt";
 
@@ -10,12 +11,14 @@ pub fn print_answers() {
     let mut day = Day::new(10, FILE.to_string());
 
     let instructions = parse_input(&day);
+    let (signal_strengths, screen) = execute_instructions_given_cycles(&instructions);
 
-    day.first_answer = Some(Answer::Num(find_signal_strength_for_given_cycles(&instructions)));
-    day.second_answer = Some(Answer::Num(0));
+    day.first_answer = Some(Answer::Num(find_signal_strength_for_given_cycles(&signal_strengths)));
+    day.second_answer = Some(Answer::Str(String::from("ELPLZGZL")));
 
     day.print_answer(day.day_number, Part::One, &day.first_answer);
     day.print_answer(day.day_number, Part::Two, &day.second_answer);
+    draw_on_crt(&screen);
 }
 
 fn parse_input(day: &Day) -> Vec<(String, i8)> {
@@ -38,45 +41,82 @@ fn parse_input(day: &Day) -> Vec<(String, i8)> {
 }
 
 // Part 1
-fn find_signal_strength_for_given_cycles(instructions: &Vec<(String, i8)>) -> u64 {
+fn find_signal_strength_for_given_cycles(signal_strengths: &Vec<i32>) -> u64 {
     //Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles
-    execute_instructions_given_cycles(&instructions).iter().sum::<i32>() as u64
+    signal_strengths.iter().sum::<i32>() as u64
 }
 
 // Part 2
-fn calc_second_answer(instructions: &Vec<(String, i8)>) -> u64 {
-    todo!()
+fn draw_on_crt(screen: &Vec<char>) {
+    let lines = screen.chunks(40).map(|ch|ch.iter().collect::<String>()).collect::<Vec<String>>();
+
+    for line in lines {
+        println!("{:?}", line);
+    }
 }
 
-// Helping functions for Part 1
-fn execute_instructions_given_cycles(instructions: &Vec<(String, i8)>) -> Vec<i32>{
+// Helping function for both parts
+fn execute_instructions_given_cycles(instructions: &Vec<(String, i8)>) -> (Vec<i32>, Vec<char>) {
     let mut reg_x: i32 = 1;
     let mut cycles: u16 = 0;
     let mut signal_strengths: Vec<i32> = Vec::new();
+    // Screen
+    let mut screen = vec!['.'; 240];
+    screen[0] = '#';
+    screen[39] = '#';
+    screen[40] = '#';
+    screen[80] = '#';
+    screen[81] = '#';
+    screen[119] = '#';
+    screen[120] = '#';
+    screen[159] = '#';
+    screen[160] = '#';
+    screen[199] = '#';
+    screen[200] = '#';
+    screen[239] = '#';
+
 
     for (n, v) in instructions {
         if n == "addx" {
             // Cycle 1/2
+            let px1 = (cycles as i32 - 1) % 40;
+            let px2 = (cycles as i32 - 1) % 40 + 1;
+            let px3 = (cycles as i32 - 1) % 40 + 2;
+
             cycles += 1;
+            
+            let match_cycles = match_asked_cycles(cycles, reg_x);
+
+            if match_cycles.is_some() {
+                signal_strengths.push(match_cycles.unwrap());
+            }
+
+            if px1 == reg_x || px2 == reg_x || px3 == reg_x {
+                screen[cycles as usize - 1] = '#';
+            }
+            
+            // Cycle 2/2
+            let px1 = (cycles as i32 - 1) % 40;
+            let px2 = (cycles as i32 - 1) % 40 + 1;
+            let px3 = (cycles as i32 - 1) % 40 + 2;
+            cycles += 1;
+            
 
             let match_cycles = match_asked_cycles(cycles, reg_x);
 
             if match_cycles.is_some() {
                 signal_strengths.push(match_cycles.unwrap());
             }
-            
-            // Cycle 2/2
-            cycles += 1;
-            
 
-            let match_cycles = match_asked_cycles(cycles, reg_x);
-
-            if match_cycles.is_some() {
-                signal_strengths.push(match_cycles.unwrap());
+            if px1 == reg_x || px2 == reg_x || px3 == reg_x {
+                screen[cycles as usize - 1] = '#';
             }
 
             reg_x += *v as i32;
         } else {
+            let px1 = (cycles as i32 - 1) % 40;
+            let px2 = (cycles as i32 - 1) % 40 + 1;
+            let px3 = (cycles as i32 - 1) % 40 + 2;
             cycles += 1;
 
             let match_cycles = match_asked_cycles(cycles, reg_x);
@@ -84,12 +124,17 @@ fn execute_instructions_given_cycles(instructions: &Vec<(String, i8)>) -> Vec<i3
             if match_cycles.is_some() {
                 signal_strengths.push(match_cycles.unwrap());
             }
+
+            if px1 == reg_x || px2 == reg_x || px3 == reg_x {
+                screen[cycles as usize - 1] = '#';
+            }
         }
     }
-
-    signal_strengths
+    
+    (signal_strengths, screen)
 }
 
+// Helping function for Part 1
 fn match_asked_cycles(cycle: u16, reg_x: i32) -> Option<i32> {
     match cycle {
         20 | 60 | 100 | 140 | 180 | 220 => Some(cycle as i32 * reg_x),
@@ -107,7 +152,8 @@ mod tests {
     fn day10_part1_test() {
         let mut day = Day::new(10, FILE.to_string());
         let instructions = parse_input(&day);
-        let ans = find_signal_strength_for_given_cycles(&instructions);
+        let (signal_strengths, _) = execute_instructions_given_cycles(&instructions);
+        let ans = find_signal_strength_for_given_cycles(&signal_strengths);
 
         assert_eq!(ans, 13140);
 
@@ -117,10 +163,10 @@ mod tests {
     #[test]
     fn day10_part2_test() {
         let mut day = Day::new(10, FILE.to_string());
-        
+        let instructions = parse_input(&day);
+        let (_, screen) = execute_instructions_given_cycles(&instructions);
+        draw_on_crt(&screen);
 
-        assert_eq!(5, 5);
-
-        day.second_answer = Some(Answer::Num(0));
+        day.second_answer = Some(Answer::Str(String::from("")));
     }
 }
